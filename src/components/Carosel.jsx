216aaaa1cc4ebd44e023f2b62500ff787291104a
Surendra Singh Kamboj <116861180+surendraSinghKamboj@users.noreply.github.com>
2023-04-23 10:57:02 +0530

    Complteted

--- src/components/Carosel.jsx
+++ src/components/Carosel.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { EffectCoverflow, Navigation, Pagination } from "swiper";
+import { EffectCoverflow, Navigation, Pagination, Autoplay } from "swiper";
 
 // Import Swiper styles
 import "swiper/css";
@@ -11,37 +11,38 @@ import carosel from "../assets/index";
 import Image from "next/image";
 
 export default function App() {
-	return (
-		<section className="carousel">
-			<Swiper
-				effect={"coverflow"}
-				grabCursor={true}
-				centeredSlides={true}
-				rewind={true}
-				slidesPerView={"auto"}
-				coverflowEffect={{
-					rotate: 0,
-					stretch: 0,
-					depth: 100,
-					modifier: 2,
-					slideShadows: true,
-				}}
-				pagination={true}
-				modules={[EffectCoverflow, Pagination, Navigation]}
-				className="swiper mt-3 mb-3"
-			>
-				{carosel &&
-					carosel.map((item, index) => {
-						return (
-							<SwiperSlide
-								key={index}
-								className="swiper-slide rounded-2xl mt-4 mb-4"
-							>
-								<Image alt="carousel" className="p-5" src={item} />
-							</SwiperSlide>
-						);
-					})}
-			</Swiper>
-		</section>
-	);
+  return (
+    <section className="carousel">
+      <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 10,
+          stretch: 0,
+          depth: 100,
+          modifier: 2,
+          slideShadows: true,
+          speed: 1000,
+        }}
+        loop={true}
+        modules={[EffectCoverflow, Pagination, Navigation, Autoplay]}
+        autoplay={{ delay: 1000 }}
+        className="swiper mt-3 mb-3"
+      >
+        {carosel &&
+          carosel.map((item, index) => {
+            return (
+              <SwiperSlide
+                key={index}
+                className="swiper-slide rounded-2xl mt-4 mb-4"
+              >
+                <Image alt="carousel" className="p-5" src={item} />
+              </SwiperSlide>
+            );
+          })}
+      </Swiper>
+    </section>
+  );
 }
