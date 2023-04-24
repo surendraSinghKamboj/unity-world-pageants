2cde9ca10d6566bac17e3d53d6ceb92a9bc137df
Surendra Singh Kamboj <116861180+surendraSinghKamboj@users.noreply.github.com>
2023-04-24 08:38:34 +0530

    bug fixes and seo fixed

---
+++ src/content.js
@@ -0,0 +1,4 @@
+export const seoDescription = `Welcome to Unity World Pageants - the premier beauty pageant that celebrates diversity, unity, and
+empowerment of women from around the world. Our pageant is more than just a competition, it is
+a platform that provides opportunities for women to showcase their talents, intellect, and beauty
+while creating lifelong bonds of sisterhood.` 
--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,14 +1,24 @@
+import Head from "next/head";
 import Link from "next/link";
 import React from "react";
+import { seoDescription } from "@/content";
 
 const Error = () => {
 	return (
-		<div className="min-h-full flex justify-center flex-col items-center">
-			<h3 className="mt-14"> Error 404 page not found</h3>
-			<h3 className="mt-2">
-				Goto <Link href={"/"}>Homepage</Link>
-			</h3>
-		</div>
+		<>
+			<Head>
+				<title>Unity World Pageants</title>
+				<meta name="description" content={seoDescription} />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<div className="min-h-full flex justify-center flex-col items-center">
+				<h3 className="mt-14"> Error 404 page not found</h3>
+				<h3 className="mt-2">
+					Goto <Link href={"/"}>Homepage</Link>
+				</h3>
+			</div>
+		</>
 	);
 };
 
