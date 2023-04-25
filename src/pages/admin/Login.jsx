d05fa74b5af1f23515935371b974c40e522ec365
Surendra Singh Kamboj <116861180+surendraSinghKamboj@users.noreply.github.com>
2023-04-25 09:20:54 +0530

    Create Admin Login.jsx

---
+++ src/pages/admin/Login.jsx
@@ -0,0 +1,14 @@
+import { motion } from "framer-motion";
+
+const Login = ()=>{
+  const [data,setData]=useState({});
+  return (
+    <>
+    <motion.div>
+      
+    </motion.div>
+    </>
+    )
+  }
+
+export default Login
