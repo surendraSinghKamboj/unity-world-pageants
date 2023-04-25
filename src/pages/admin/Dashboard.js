2aa7e8f52dad4dbfb96c6f94a0bbd7171a5a871f
Surendra Singh Kamboj <116861180+surendraSinghKamboj@users.noreply.github.com>
2023-04-25 12:00:44 +0530

    Create Dashboard.js

---
+++ src/pages/admin/Dashboard.js
@@ -0,0 +1,12 @@
+import {motion} from "framer-motion"; 
+
+
+const Dashboard =()=>{
+   return (
+     <>
+        <motion.div>
+           <h3>Dashboard </h3>
+        </motion.div>
+     </>
+   )
+}
